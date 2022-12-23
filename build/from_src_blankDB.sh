b13be94cb26b64efe299f6c87eb12d1be7540cfb
himesb <himes.benjamin@gmail.com>
2022-12-23 08:27:52 -0500

    Looks like yarn installed locally as USER so switch everything up to use root

--- build/from_src_blankDB.sh
+++ build/from_src_blankDB.sh
@@ -9,7 +9,7 @@
 
 # Prepared from https://docs.joinmastodon.org/admin/prerequisites/
 
-# Assuming a clean Ubuntu 20.04 LTS install and a user with sudo privileges added eg: user ALL=(ALL) NOPASSWD:ALL
+# Assuming a clean Ubuntu 20.04 LTS install and a user with privileges added eg: user ALL=(ALL) NOPASSWD:ALL
 
 # First make sure you are actually logging in to the server using keys and not via a password, otherwise this will lock you out. 
 # Many hosting providers support uploading a public key and automatically set up key-based root login on new machines for you
@@ -20,7 +20,6 @@ admin_email="Benjamin.Himes@${domain_name}"
 
 set -e
 
-
 echo -e "Important: are you sure you are set up for key based login? the next step will lock you out if you are not. (y/n)"
 read -r answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
@@ -30,16 +29,19 @@ else
     exit 1
 fi
 
+# Change to root
+sudo su -
+
 # Okay, get rid of password based ssh
-sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
-awk '{if(/^#PasswordAuthentication / || /PasswordAuthentication /) print "PasswordAuthentication no" ; else print $0}' /etc/ssh/sshd_config.bak | sudo tee /etc/ssh/sshd_config
-sudo systemctl restart ssh.service
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+awk '{if(/^#PasswordAuthentication / || /PasswordAuthentication /) print "PasswordAuthentication no" ; else print $0}' /etc/ssh/sshd_config.bak | tee /etc/ssh/sshd_config
+systemctl restart ssh.service
 
 # Update the system
-sudo apt update && sudo apt upgrade -y
+apt update && apt upgrade -y
 
 # Install fail2ban to prevent brute force attacks
-sudo apt install fail2ban -y
+apt install fail2ban -y
 tmp_fail2ban=$(mktemp)
 {
 echo '[DEFAULT]'
@@ -56,13 +58,13 @@ echo 'port = 22'
 } > $tmp_fail2ban
 
 
-cat $tmp_fail2ban | sudo tee /etc/fail2ban/jail.local
+cat $tmp_fail2ban | tee /etc/fail2ban/jail.local
 rm $tmp_fail2ban
 
-sudo systemctl restart fail2ban
+systemctl restart fail2ban
 
 # Setup a software firewall
-sudo apt install -y iptables-persistent
+apt install -y iptables-persistent
 
 tmp_iptables=$(mktemp)
 {
@@ -102,10 +104,10 @@ echo ''
 echo 'COMMIT'
 } > $tmp_iptables
 
-cat $tmp_iptables | sudo tee /etc/iptables/rules.v4
+cat $tmp_iptables | tee /etc/iptables/rules.v4
 rm $tmp_iptables
 
-sudo iptables-restore < /etc/iptables/rules.v4
+iptables-restore < /etc/iptables/rules.v4
 
 # Rules for IPv6 as well
 tmp_iptables=$(mktemp)
@@ -143,29 +145,27 @@ echo ''
 echo 'COMMIT'
 } > $tmp_iptables
 
-cat $tmp_iptables | sudo tee /etc/iptables/rules.v6
+cat $tmp_iptables | tee /etc/iptables/rules.v6
 rm $tmp_iptables
 
-sudo ip6tables-restore < /etc/iptables/rules.v6
+ip6tables-restore < /etc/iptables/rules.v6
 
 
 # On to installing the needed software
 
 # First required packages
-sudo apt install -y curl wget gnupg apt-transport-https lsb-release ca-certificates
+apt install -y curl wget gnupg apt-transport-https lsb-release ca-certificates
 
 # Add the nodejs repo
-sudo -i -u himesb bash << EOF
-curl -sL https://deb.nodesource.com/setup_16.x 
-EOF
+curl -sL https://deb.nodesource.com/setup_16.x | bash -
 
-sudo wget -O /usr/share/keyrings/postgresql.asc https://www.postgresql.org/media/keys/ACCC4CF8.asc
-echo "deb [signed-by=/usr/share/keyrings/postgresql.asc] http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" | sudo tee /etc/apt/sources.list.d/postgresql.list
+wget -O /usr/share/keyrings/postgresql.asc https://www.postgresql.org/media/keys/ACCC4CF8.asc
+echo "deb [signed-by=/usr/share/keyrings/postgresql.asc] http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" | tee /etc/apt/sources.list.d/postgresql.list
 
 # More system packages
 
-sudo apt update
-sudo apt install -y \
+apt update
+apt install -y \
      imagemagick ffmpeg libpq-dev libxml2-dev libxslt1-dev file git-core \
      g++ libprotobuf-dev protobuf-compiler pkg-config nodejs gcc autoconf \
      bison build-essential libssl-dev libyaml-dev libreadline6-dev \
@@ -180,10 +180,10 @@ yarn set version classic
 # We will be using rbenv to manage Ruby versions, because it’s easier to get the right versions and to update once a newer release comes out. 
 # rbenv must be installed for a single Linux user, therefore, first we must create the user Mastodon will be running as:
 
-sudo adduser --disabled-login mastodon
+adduser --disabled-login mastodon
 
 # Next, we want to do a few things as the mastodon user:
-sudo -i -u mastodon bash << EOF
+su -i -u mastodon bash << EOF
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 cd ~/.rbenv && src/configure && make -C src
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
@@ -198,13 +198,13 @@ EOF
 # Now on to some setup work
 
 # Create a DB user for Mastodon with ident authentication so mastodon can connect to the DB
-sudo -u postgres psql << EOF
+su -u postgres psql << EOF
 CREATE USER mastodon CREATEDB;
 \q
 EOF
 
 # Mastodon setup as mastodon user
-sudo -i -u mastodon bash << EOF
+su -i -u mastodon bash << EOF
 git clone https://github.com/mastodon/mastodon.git live && cd live
 git checkout $(git tag -l | grep -v 'rc[0-9]*$' | sort -V | tail -n 1)
 bundle config deployment 'true'
@@ -219,18 +219,18 @@ EOF
 cp /home/mastodon/live/dist/nginx.conf /etc/nginx/sites-available/mastodon
 ln -s /etc/nginx/sites-available/mastodon /etc/nginx/sites-enabled/mastodon
 
-sudo mv /etc/nginx/sites-available/mastodon /etc/nginx/sites-available/mastodon.bak
-awk -v WANTED_DOMAIN=$domain_name '{gsub("example.com",WANTED_DOMAIN)}{print $0}' /etc/nginx/sites-available/mastodon.bak | sudo tee /etc/nginx/sites-available/mastodon
+mv /etc/nginx/sites-available/mastodon /etc/nginx/sites-available/mastodon.bak
+awk -v WANTED_DOMAIN=$domain_name '{gsub("example.com",WANTED_DOMAIN)}{print $0}' /etc/nginx/sites-available/mastodon.bak | tee /etc/nginx/sites-available/mastodon
 
-sudo systemctl reload nginx
+systemctl reload nginx
 
 certbot --nginx -d $domain_name
-sudo cp /home/mastodon/live/dist/mastodon-*.service /etc/systemd/system/
+cp /home/mastodon/live/dist/mastodon-*.service /etc/systemd/system/
 
 #If you deviated from the defaults at any point, check that the username and paths are correct:
 # This is vanilla af
 # # $EDITOR /etc/systemd/system/mastodon-*.service
 
-sudo systemctl daemon-reload
-sudo systemctl enable --now mastodon-web mastodon-sidekiq mastodon-streaming
+systemctl daemon-reload
+systemctl enable --now mastodon-web mastodon-sidekiq mastodon-streaming
 

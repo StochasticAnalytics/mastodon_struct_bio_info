cd0ff4d5e84b987d001fb7ae7164b1e24ea31792
himesb <himes.benjamin@gmail.com>
2022-12-23 08:12:35 -0500

    Fixes text in fail2ban config

--- build/from_src_blankDB.sh
+++ build/from_src_blankDB.sh
@@ -30,7 +30,7 @@ fi
 
 # Okay, get rid of password based ssh
 sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
-awk '{if(/^#PasswordAuthentication / || /PasswordAuthentication /) print $0}' | sudo tee /etc/ssh/sshd_config
+awk '{if(/^#PasswordAuthentication / || /PasswordAuthentication /) print "PasswordAuthentication no" ; else print $0}' | sudo tee /etc/ssh/sshd_config
 sudo systemctl restart ssh.service
 
 # Update the system
@@ -40,17 +40,17 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install fail2ban -y
 tmp_fail2ban=$(mktemp)
 {
-[DEFAULT]
-destemail = ${admin_email}
-sendername = Fail2Ban
-
-[sshd]
-enabled = true
-port = 22
-
-[sshd-ddos]
-enabled = true
-port = 22
+echo '[DEFAULT]'
+echo 'destemail = ${admin_email}'
+echo 'sendername = Fail2Ban'
+echo ''
+echo '[sshd]'
+echo 'enabled = true'
+echo 'port = 22'
+echo ''
+echo '[sshd-ddos]'
+echo 'enabled = true'
+echo 'port = 22'
 } > $tmp_fail2ban
 
 
@@ -64,40 +64,40 @@ sudo apt install -y iptables-persistent
 
 tmp_iptables=$(mktemp)
 {
-*filter
-
-#  Allow all loopback (lo0) traffic and drop all traffic to 127/8 that doesn't use lo0
--A INPUT -i lo -j ACCEPT
--A INPUT ! -i lo -d 127.0.0.0/8 -j REJECT
-
-#  Accept all established inbound connections
--A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-
-#  Allow all outbound traffic - you can modify this to only allow certain traffic
--A OUTPUT -j ACCEPT
-
-#  Allow HTTP and HTTPS connections from anywhere (the normal ports for websites and SSL).
--A INPUT -p tcp --dport 80 -j ACCEPT
--A INPUT -p tcp --dport 443 -j ACCEPT
-
-#  Allow SSH connections
-#  The -dport number should be the same port number you set in sshd_config
--A INPUT -p tcp -m state --state NEW --dport 22 -j ACCEPT
-
-#  Allow ping
--A INPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT
-
-# Allow destination unreachable messages, especially code 4 (fragmentation required) is required or PMTUD breaks
--A INPUT -p icmp -m icmp --icmp-type 3 -j ACCEPT
-
-#  Log iptables denied calls
--A INPUT -m limit --limit 5/min -j LOG --log-prefix "iptables denied: " --log-level 7
-
-#  Reject all other inbound - default deny unless explicitly allowed policy
--A INPUT -j REJECT
--A FORWARD -j REJECT
-
-COMMIT
+echo '*filter'
+echo ''
+echo '#  Allow all loopback (lo0) traffic and drop all traffic to 127/8 that does not use lo0'
+echo '-A INPUT -i lo -j ACCEPT'
+echo '-A INPUT ! -i lo -d 127.0.0.0/8 -j REJECT'
+echo ''
+echo '#  Accept all established inbound connections'
+echo '-A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT'
+echo ''
+echo '#  Allow all outbound traffic - you can modify this to only allow certain traffic'
+echo '-A OUTPUT -j ACCEPT'
+echo ''
+echo '#  Allow HTTP and HTTPS connections from anywhere (the normal ports for websites and SSL).'
+echo '-A INPUT -p tcp --dport 80 -j ACCEPT'
+echo '-A INPUT -p tcp --dport 443 -j ACCEPT'
+echo ''
+echo '#  Allow SSH connections'
+echo '#  The -dport number should be the same port number you set in sshd_config'
+echo '-A INPUT -p tcp -m state --state NEW --dport 22 -j ACCEPT'
+echo ''
+echo '#  Allow ping'
+echo '-A INPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT'
+echo ''
+echo '# Allow destination unreachable messages, especially code 4 (fragmentation required) is required or PMTUD breaks'
+echo '-A INPUT -p icmp -m icmp --icmp-type 3 -j ACCEPT'
+echo ''
+echo '#  Log iptables denied calls'
+echo '-A INPUT -m limit --limit 5/min -j LOG --log-prefix "iptables denied: " --log-level 7'
+echo ''
+echo '#  Reject all other inbound - default deny unless explicitly allowed policy'
+echo '-A INPUT -j REJECT'
+echo '-A FORWARD -j REJECT'
+echo ''
+echo 'COMMIT'
 } > $tmp_iptables
 
 cat $tmp_iptables | sudo tee /etc/iptables/rules.v4
@@ -108,37 +108,37 @@ sudo iptables-restore < /etc/iptables/rules.v4
 # Rules for IPv6 as well
 tmp_iptables=$(mktemp)
 {
-*filter
-
-#  Allow all loopback (lo0) traffic and drop all traffic to 127/8 that doesn't use lo0
--A INPUT -i lo -j ACCEPT
--A INPUT ! -i lo -d ::1/128 -j REJECT
-
-#  Accept all established inbound connections
--A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-
-#  Allow all outbound traffic - you can modify this to only allow certain traffic
--A OUTPUT -j ACCEPT
-
-#  Allow HTTP and HTTPS connections from anywhere (the normal ports for websites and SSL).
--A INPUT -p tcp --dport 80 -j ACCEPT
--A INPUT -p tcp --dport 443 -j ACCEPT
-
-#  Allow SSH connections
-#  The -dport number should be the same port number you set in sshd_config
--A INPUT -p tcp -m state --state NEW --dport 22 -j ACCEPT
-
-#  Allow ping
--A INPUT -p icmpv6 -j ACCEPT
-
-#  Log iptables denied calls
--A INPUT -m limit --limit 5/min -j LOG --log-prefix "iptables denied: " --log-level 7
-
-#  Reject all other inbound - default deny unless explicitly allowed policy
--A INPUT -j REJECT
--A FORWARD -j REJECT
-
-COMMIT
+echo '*filter'
+echo ''
+echo '#  Allow all loopback (lo0) traffic and drop all traffic to 127/8 that does not use lo0'
+echo '-A INPUT -i lo -j ACCEPT'
+echo '-A INPUT ! -i lo -d ::1/128 -j REJECT'
+echo ''
+echo '#  Accept all established inbound connections'
+echo '-A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT'
+echo ''
+echo '#  Allow all outbound traffic - you can modify this to only allow certain traffic'
+echo '-A OUTPUT -j ACCEPT'
+echo ''
+echo '#  Allow HTTP and HTTPS connections from anywhere (the normal ports for websites and SSL).'
+echo '-A INPUT -p tcp --dport 80 -j ACCEPT'
+echo '-A INPUT -p tcp --dport 443 -j ACCEPT'
+echo ''
+echo '#  Allow SSH connections'
+echo '#  The -dport number should be the same port number you set in sshd_config'
+echo '-A INPUT -p tcp -m state --state NEW --dport 22 -j ACCEPT'
+echo ''
+echo '#  Allow ping'
+echo '-A INPUT -p icmpv6 -j ACCEPT'
+echo ''
+echo '#  Log iptables denied calls'
+echo '-A INPUT -m limit --limit 5/min -j LOG --log-prefix "iptables denied: " --log-level 7'
+echo ''
+echo '#  Reject all other inbound - default deny unless explicitly allowed policy'
+echo '-A INPUT -j REJECT'
+echo '-A FORWARD -j REJECT'
+echo ''
+echo 'COMMIT'
 } > $tmp_iptables
 
 cat $tmp_iptables | sudo tee /etc/iptables/rules.v6
